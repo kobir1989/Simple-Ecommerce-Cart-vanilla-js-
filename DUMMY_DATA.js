e30f25bb15c762120251b54bd471bd5f2ccb903e
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-09-28 11:47:57 +0600

    Project Initialized

---
+++ DUMMY_DATA.js
@@ -0,0 +1,75 @@
+const productData = [
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Beats model a1796',
+    imgUrl: './img/headphone1.png',
+    price: '400',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Sony model d1796',
+    imgUrl: './img/headphone2.jpg',
+    price: '299',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Beat new model a1746',
+    imgUrl: './img/headphone3.webp',
+    price: '180',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'HyperX model F1796',
+    imgUrl: './img/headphone4.jpg',
+    price: '89',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'T-shirt men 2022',
+    imgUrl: './img/tshirt1.jpg',
+    price: '29',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'T-shirt men gray',
+    imgUrl: './img/tshirt2.webp',
+    price: '25',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'T-shirt men black',
+    imgUrl: './img/tshirt3.jpg',
+    price: '22',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'T-shirt men new',
+    imgUrl: './img/tshirt4.jpg',
+    price: '39',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Nike 2022 running',
+    imgUrl: './img/nike-1.webp',
+    price: '180',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Nike air z-1100',
+    imgUrl: './img/nike-2.webp',
+    price: '220',
+  },
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Nike air-max bolt',
+    imgUrl: './img/nike-3.webp',
+    price: '350',
+  },
+
+  {
+    id: Math.floor(Math.random() * 10000),
+    name: 'Nike air jordan',
+    imgUrl: './img/nike-4.webp',
+    price: '299',
+  },
+];
